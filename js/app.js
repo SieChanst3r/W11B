2f14918881531f7c2972643a4a4cb4910a527c28
SieChan <sie.chan@outlook.com>
2020-09-04 15:41:24 -0600

    debugging

--- js/app.js
+++ js/app.js
@@ -16,21 +16,47 @@
 //     ajax.send();
 // }
 
+// function getGiphy() {
+//     let ajax = new XMLHttpRequest();
+//     ajax.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+
+//             let giphy = JSON.parse(this.responseText);
+//             document.getElementById("giphy").innerHTML = giphy.URL;
+//         } else if (this.readyState != 4) {
+//             document.getElementById("giphy").innerHTML = "LOADING...";
+//         } else {
+//             document.getElementById("giphy").innerHTML = "Something went wrong...";
+//         }
+//     }
+//     ajax.open("GET", "https://random.dog/woof.json", true);
+//     ajax.send();
+// }
+
+// document.getElementById("giphy").addEventListener('click', getGiphy);
+
 function getGiphy() {
     let ajax = new XMLHttpRequest();
     ajax.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
 
             let giphy = JSON.parse(this.responseText);
-            document.getElementById("giphy").innerHTML = giphy.giphy;
+
+            // I believe this object that you have called giphy does not have a "giphy" key. I'm pretty sure the key is called "url" meaning you should have giphy.url
+            document.getElementById("giphy") = giphy.URL;
+            // But also, I'mprety sure you don't want the url to be the innerHTML. We did an example on how to set an attribute on a tag before
+
         } else if (this.readyState != 4) {
             document.getElementById("giphy").innerHTML = "LOADING...";
         } else {
             document.getElementById("giphy").innerHTML = "Something went wrong...";
         }
     }
-    ajax.open("GET", "https://random.dog/e3bb196a-0153-4122-8725-24a085404495.gif", true);
+
+    // Here, the address of the API is actually "https://random.dog/woof.json" You have the address of a gif here
+    ajax.open("GET", "https://random.dog/woof.json", true);
     ajax.send();
 }
 
-document.getElementById("giphy").addEventListener('click', giphy);
+// The second argument to the addEventListener needs to be the NAME of the FUNCTION you want to call.
+document.getElementById("giphy").addEventListener('click', getGiphy());
